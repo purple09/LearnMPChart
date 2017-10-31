264627ff4dc581c91f01eba698ae018f3bcbe016
purple <guizhen@smm.cn>
2017-10-31 11:16:01 +0800

    添加/修改了很多功能

---
+++ app/src/main/java/gz/smm/mpchart/SimpleGridData.java
@@ -0,0 +1,12 @@
+package gz.smm.mpchart;
+
+/**
+ * Created by zhangdi on 17/6/16.
+ */
+
+public class SimpleGridData {
+    public String position;
+    public String show;
+    public double dashed_line;
+    public double percentage;
+}
